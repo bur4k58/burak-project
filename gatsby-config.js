eb97536d7a6548813c4f7e7232d473cc9a4b35a5
bur4k58 <s116768@ap.be>
2020-11-24 16:37:32 +0100

    les

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,34 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Burak Project`,
+    description: `Project`,
+    author: `Burak`,
   },
   plugins: [
+    {
+
+      resolve: "gatsby-source-graphql",
+      
+      options: {
+      
+      typeName: "WPGraphql",
+      
+      fieldName: "wpcontent",
+      
+      url: "http://burakproject.local/graphql",
+      
+      },
+      
+      },
+      {
+        resolve: `gatsby-plugin-google-fonts`,
+        options: {
+        fonts: [`Roboto`, `Oswald`],
+        display: "swap",
+        },
+        }, 
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
